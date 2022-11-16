efd763f335c62e5d84e5cd1b689a6dba278b7f18
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2022-11-16 15:38:01 -0600

    Tarea de Coursera

---
+++ Go/makejson.go
@@ -0,0 +1,12 @@
+package main
+
+import ("fmt")
+
+func main(){
+    var name, address = string, string
+    fmt.Printf("Write your name: ")
+    fmt.Scan(&name)
+    fmt.Printf("Write yout address: ")
+    fmt.Scan(&address)
+    fmt.Printf("Hola Mundo!\n")
+}

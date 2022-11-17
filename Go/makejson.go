67e25feaab420044294030ed6ecb1013c9761247
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2022-11-16 20:01:17 -0600

    Avance

--- Go/makejson.go
+++ Go/makejson.go
@@ -1,12 +1,31 @@
 package main
 
-import ("fmt")
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
 
-func main(){
-    var name, address = string, string
-    fmt.Printf("Write your name: ")
-    fmt.Scan(&name)
-    fmt.Printf("Write yout address: ")
-    fmt.Scan(&address)
-    fmt.Printf("Hola Mundo!\n")
+func main() {
+	var name, address string
+	fmt.Printf("Write your name: ")
+	fmt.Scan(&name)
+	fmt.Printf("Write yout address: ")
+	reader := bufio.NewReader(os.Stdin)
+	address := bufio.NewScanner(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("\nYour name is %v and you live in %v\n", name, address)
+
+	m := make(map[string]string)
+	m["name"] = name
+	m["address"] = address
+
+	j, _ := json.Marshal(m)
+	fmt.Println(m)
+	fmt.Println(string(j))
 }

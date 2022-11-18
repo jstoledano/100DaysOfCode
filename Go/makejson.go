8e65361902882ad90440631cf250cf7fe70864ec
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2022-11-17 21:05:40 -0600

    Primer curso terminado

--- Go/makejson.go
+++ Go/makejson.go
@@ -4,28 +4,26 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
+	// Variables
 	var name, address string
+
+	// Prompt for name and address
 	fmt.Printf("Write your name: ")
 	fmt.Scan(&name)
 	fmt.Printf("Write yout address: ")
 	reader := bufio.NewReader(os.Stdin)
-	address := bufio.NewScanner(os.Stdin)
-	if err != nil {
-		log.Fatal(err)
-	}
+	address, _ = reader.ReadString(('\n'))
 
-	fmt.Printf("\nYour name is %v and you live in %v\n", name, address)
-
+	// Making a map
 	m := make(map[string]string)
 	m["name"] = name
 	m["address"] = address
 
+	// Create the json
 	j, _ := json.Marshal(m)
-	fmt.Println(m)
 	fmt.Println(string(j))
 }
